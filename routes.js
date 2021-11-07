d1e636c783c7f1cddc763dcfc678e9a325386dc4
Nguyen Ngoc Duc <55783340+nnduc2207@users.noreply.github.com>
2021-11-07 22:57:14 +0700

    setup index

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 'use strict'
 const classRoute = require('./components/class/router')
 const userRoute = require('./components/user/router')
-module.exports = function (app, opts) {
+module.exports = function (app) {
   // Setup routes, middleware, and handlers
   app.get('/', (req, res) => {
     res.locals.name = '.'
